ab26325adad9c98ad5291e8b8ebdab873e132d28
Mortaza Zolfpour Arokhlo <mortazazolfpour@gmail.com>
2025-11-12 10:41:02 +0000

    remove index.html

---
+++ .env.sh
@@ -0,0 +1,3 @@
+DEBUG=False
+SECRET_KEY=django-insecure-chinook-secret-key-2024
+DATABASE_URL=postgresql://neondb_owner:npg_FgsZR9MWb8Sr@ep-long-dust-aggdj4tj.c-2.eu-central-1.aws.neon.tech/alive_smell_rank_585650
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn profile_test.wsgi --log-file -
